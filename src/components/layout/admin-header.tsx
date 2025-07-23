a3633e5e0e6a5083af85f7be646730b8c79b2576
Lijo J S - iRoid <lijo.js@iroidtechnologies.com>
2025-07-23 09:39:51 +0000

    side bar required toggle and nav bar

--- src/components/layout/admin-header.tsx
+++ src/components/layout/admin-header.tsx
@@ -16,7 +16,7 @@ import { SidebarTrigger } from '../ui/sidebar';
 export function AdminHeader() {
   return (
     <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
-       <SidebarTrigger className="sm:hidden" />
+       <SidebarTrigger />
       <div className="relative ml-auto flex-1 md:grow-0">
         {/* Can add a search bar here later if needed */}
       </div>
